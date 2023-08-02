ecdb0c4939183345fb5adfb41cbdf1732c3a16f6
SwathedSplash62 <138179868+SwathedSplash62@users.noreply.github.com>
2023-08-03 10:03:25 +1200

    Add files via upload

--- 03_Bit_Calc.py
+++ 03_Bit_Calc.py
@@ -7,7 +7,7 @@ def statement_generator(text, decoration):
     ends = decoration * 5
 
     # add decoration to start and end of statement
-    statement = "{}{} {} {}{}".format(ends, ends, text, ends, ends)
+    statement = "{} {} {}".format(ends, text, ends)
 
     print()
     print(statement)
@@ -15,5 +15,20 @@ def statement_generator(text, decoration):
 
     return ""
 
-statement_generator("Bit Calculator", "*")
+statement_generator("Hello World", "-")
 
+def user_choice():
+
+    valid = False
+    while not valid:
+
+        response = "File type (integer / text / image): ".lower()
+
+        if response == "text" or response == "t":
+            return response
+
+        else:
+            print("Please choose a vaild file type :((")
+            print()
+
+data_type = user_choice()
