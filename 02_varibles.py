f8b0dc438f3ddfed025a8a7f38436da6609dac79
SwathedSplash62 <138179868+SwathedSplash62@users.noreply.github.com>
2023-08-03 09:54:44 +1200

    Add files via upload

---
+++ 02_varibles.py
@@ -0,0 +1,12 @@
+#code to learn about variables
+
+#varibles can be a string
+
+#they can also be numbers
+fav_num = 8
+#this  variable has a problem
+my_name = "KY"
+print(fav_num*2)
+print(my_name)
+num_string = "5"
+print(num_string*5)
